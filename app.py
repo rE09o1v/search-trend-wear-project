e3b39f2b06c66b48ed117142824926a93b8326c0
rE09o1v <daiki.mebiusu1205@gmail.com>
2025-05-26 14:39:58 +0900

    Update app.py
    plotlyのインポートにfromが抜けていた

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
+from import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import json
 from pathlib import Path
