28fb3a6e86e96a412380b914b238a96e523b077a
rE09o1v <daiki.mebiusu1205@gmail.com>
2025-05-26 14:50:48 +0900

    Update app.py
    元の形に修正

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from plotly import graph_objects as go
+import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import json
 from pathlib import Path
