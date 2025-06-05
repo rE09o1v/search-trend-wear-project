2340f12c3909d8362eda618361c1c17ae954d715
rE09o1v <daiki.mebiusu1205@gmail.com>
2025-06-05 13:16:04 +0900

    Github Actionsで自動的にスクレイピングするように変更 batファイルを実行してgit pullをしてからapp.pyを実行するフローを自動化

---
+++ start_app.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM プロジェクトのルートディレクトリに移動する (実際のパスに置き換えてください)
+cd /d "C:\python-projects\search-trend-wear-project"
+
+echo 最新のデータを取得しています (git pull)...
+git pull
+
+echo Streamlitアプリケーションを起動します...
+streamlit run app.py
