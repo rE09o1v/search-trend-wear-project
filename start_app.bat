9aa33667fb12a379a276fe2b628825ec6f2701af
rE09o1v <daiki.mebiusu1205@gmail.com>
2025-07-22 14:57:46 +0900

    batファイルに仮想環境のアクティベートを追加

--- start_app.bat
+++ start_app.bat
@@ -3,6 +3,9 @@
 REM プロジェクトのルートディレクトリに移動する (実際のパスに置き換えてください)
 cd /d "C:\python-projects\search-trend-wear-project"
 
+REM 仮想環境をアクティブ化
+call venv\Scripts\activate
+
 echo 最新のデータを取得しています (git pull)...
 git pull
 
